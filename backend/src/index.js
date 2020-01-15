769532a556f2c2c747cc506d9deb7087a8ec814a
Douglas Canevarollo <douglascanevarollo@gmail.com>
2020-01-15 11:56:24 -0300

    Functionalities done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://dcanevarollo:j672fb3i@omnistack-1nrd1.mongodb.ne
   useUnifiedTopology: true
 });
 
+app.use(cors({ origin: 'http://localhost:3000' }));  // Authorizes the React app to access the API.
 app.use(express.json());
 app.use(routes);
 
--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
@@ -6,6 +7,56 @@ import './Sidebar.css';
 import './Main.css';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [github_username, setGitHubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (error) => {
+        console.log(error);
+      },
+      {
+        timeout: 30000
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+
+    setGitHubUsername('');
+    setTechs('');
+
+    setDevs([...devs, response.data.newDev]);
+  }
+
   return (
     <div id="app">
       <aside>
@@ -14,96 +65,73 @@ function App() {
         <form>
           <div className="input-block">
             <label htmlFor="github_username">Usuário do GitHub</label>
-            <input name="github_username" id="github_username" required />
+            <input 
+              name="github_username" 
+              id="github_username" 
+              required 
+              value={github_username}
+              onChange={e => setGitHubUsername(e.target.value)}  
+            />
           </div>
 
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input 
+              name="techs" 
+              id="techs" 
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)} 
+            />
           </div>
 
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input 
+                type="number" 
+                name="latitude" 
+                id="latitude" 
+                required 
+                value={latitude} 
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+              <input 
+                type="number" 
+                name="longitude" 
+                id="longitude" 
+                required 
+                value={longitude} 
+                onChange={e => setLongitude(e.target.value)}  
+              />
             </div>
           </div>
 
-          <button type="submit">Salvar</button>
+          <button type="submit" onClick={handleAddDev}>Salvar</button>
         </form>
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/44115390?s=460&v=4" alt="Douglas Canevarollo" />
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
+              <header>
+                <img src={dev.avatar_url} alt={dev.name} />
 
-              <div className="user-info">
-                <strong>Douglas Canevarollo</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
+              </header>
 
-            <p>
-              Computer science student in São Paulo State University - UNESP. Also a researcher and software developer 
-              on GBD.
-            </p>
-            <a href="https://github.com/dcanevarollo">Acessar perfil do GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/44115390?s=460&v=4" alt="Douglas Canevarollo" />
-
-              <div className="user-info">
-                <strong>Douglas Canevarollo</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-
-            <p>
-              Computer science student in São Paulo State University - UNESP. Also a researcher and software developer 
-              on GBD.
-            </p>
-            <a href="https://github.com/dcanevarollo">Acessar perfil do GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/44115390?s=460&v=4" alt="Douglas Canevarollo" />
-
-              <div className="user-info">
-                <strong>Douglas Canevarollo</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-
-            <p>
-              Computer science student in São Paulo State University - UNESP. Also a researcher and software developer 
-              on GBD.
-            </p>
-            <a href="https://github.com/dcanevarollo">Acessar perfil do GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/44115390?s=460&v=4" alt="Douglas Canevarollo" />
-
-              <div className="user-info">
-                <strong>Douglas Canevarollo</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-
-            <p>
-              Computer science student in São Paulo State University - UNESP. Also a researcher and software developer 
-              on GBD.
-            </p>
-            <a href="https://github.com/dcanevarollo">Acessar perfil do GitHub</a>
-          </li>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil do GitHub</a>
+            </li>
+          ))}
         </ul>
       </main>
     </div>
