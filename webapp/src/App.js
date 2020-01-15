584e5bd27117162e61033606b1f482ffa8c07dd3
Douglas Canevarollo <douglascanevarollo@gmail.com>
2020-01-15 14:31:30 -0300

    Componentization

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,33 +3,12 @@ import api from './services/api';
 
 import './global.css';
 import './App.css';
-import './Sidebar.css';
-import './Main.css';
+
+import Sidebar from './components/Sidebar';
+import Main from './components/Main';
 
 function App() {
   const [devs, setDevs] = useState([]);
-
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-  const [github_username, setGitHubUsername] = useState('');
-  const [techs, setTechs] = useState('');
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (error) => {
-        console.log(error);
-      },
-      {
-        timeout: 30000
-      }
-    );
-  }, []);
 
   useEffect(() => {
     async function loadDevs() {
@@ -41,99 +20,10 @@ function App() {
     loadDevs();
   }, []);
 
-  async function handleAddDev(e) {
-    e.preventDefault();
-
-    const response = await api.post('/devs', {
-      github_username,
-      techs,
-      latitude,
-      longitude
-    });
-
-    setGitHubUsername('');
-    setTechs('');
-
-    setDevs([...devs, response.data.newDev]);
-  }
-
   return (
     <div id="app">
-      <aside>
-        <strong>Cadastrar</strong>
-
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do GitHub</label>
-            <input 
-              name="github_username" 
-              id="github_username" 
-              required 
-              value={github_username}
-              onChange={e => setGitHubUsername(e.target.value)}  
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input 
-              name="techs" 
-              id="techs" 
-              required
-              value={techs}
-              onChange={e => setTechs(e.target.value)} 
-            />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input 
-                type="number" 
-                name="latitude" 
-                id="latitude" 
-                required 
-                value={latitude} 
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input 
-                type="number" 
-                name="longitude" 
-                id="longitude" 
-                required 
-                value={longitude} 
-                onChange={e => setLongitude(e.target.value)}  
-              />
-            </div>
-          </div>
-
-          <button type="submit" onClick={handleAddDev}>Salvar</button>
-        </form>
-      </aside>
-
-      <main>
-        <ul>
-          {devs.map(dev => (
-            <li key={dev._id} className="dev-item">
-              <header>
-                <img src={dev.avatar_url} alt={dev.name} />
-
-                <div className="user-info">
-                  <strong>{dev.name}</strong>
-                  <span>{dev.techs.join(', ')}</span>
-                </div>
-              </header>
-
-              <p>{dev.bio}</p>
-              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil do GitHub</a>
-            </li>
-          ))}
-        </ul>
-      </main>
+      <Sidebar devs={devs} setDevs={setDevs} />
+      <Main devs={devs} />
     </div>
   );
 }
